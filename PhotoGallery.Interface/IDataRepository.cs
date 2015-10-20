b6206d984da409d67531961baa0fc6a03587f449
ebube <ebiigweze@yahoo.com>
2015-10-20 21:15:55 +0100

    adds DataRepository Interface (IDataRepository) to the project

---
+++ PhotoGallery.Interface/IDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Service
+{
+    public interface IDataRepository
+    {
+        IQueryable<T> Query<T>();
+        IQueryable<T> Query<T>(params Expression<Func<T, object>>[] include);
+        T GetByID<T>(int id);
+        void Add<T>(T item);
+        void Delete<T>(T item);
+        void Update<T>(T item);
+        void SaveChanges();
+    }
+}
