3d95621f6c0aaae91053a6d58a3ffb829345c401
ebube <ebiigweze@yahoo.com>
2015-10-21 12:24:03 +0100

    adds Interface for the GalleryManager (IGalleryManager) to Interface

---
+++ PhotoGallery.Interface/IGalleryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Interface
+{
+    public interface IGalleryManager
+    {
+        List<IGallery> GetGalleries();
+        void AddGallery(IGallery gal);
+        void RemoveGallery(IGallery gal);
+        void UpdateGallery(IGallery gal);
+        void AddPhoto(IPhoto photo);
+        void RemovePhoto(IPhoto photo);
+    }
+}
