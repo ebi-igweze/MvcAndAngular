390366f4c85b3b1727d5b214e4df5419a6c75283
ebube <ebiigweze@yahoo.com>
2015-10-24 17:51:30 +0100

    adds migration for the User Identity DbContext

--- PhotoGallery.Data/UserRole.cs
+++ PhotoGallery.Data/UserRole.cs
@@ -11,5 +11,8 @@ namespace PhotoGallery.Data
         public int Id { get; set; }
         public int RoleId { get; set; }
         public int UserId { get; set; }
+
+        public virtual Role Role { get; set; }
+        public virtual User User { get; set; }
     }
 }
